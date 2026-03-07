7903dd5c9cc7e47d14f921a99f16337fd63829f0
Thiago Kenji Okada <thiagokokada@gmail.com>
2026-03-07 21:29:44 +0000

    Polish permission rationale screen

--- app/src/main/java/com/github/thiagokokada/omronsyncer/HealthConnectPermissionsRationaleActivity.kt
+++ app/src/main/java/com/github/thiagokokada/omronsyncer/HealthConnectPermissionsRationaleActivity.kt
@@ -2,6 +2,9 @@ package com.github.thiagokokada.omronsyncer
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import com.github.thiagokokada.omronsyncer.databinding.ActivityHealthConnectPermissionsRationaleBinding
 
 class HealthConnectPermissionsRationaleActivity : AppCompatActivity() {
@@ -13,9 +16,30 @@ class HealthConnectPermissionsRationaleActivity : AppCompatActivity() {
 
         binding = ActivityHealthConnectPermissionsRationaleBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        applyWindowInsets()
 
         binding.closeButton.setOnClickListener {
             finish()
         }
+    }
+
+    private fun applyWindowInsets() {
+        val contentPaddingLeft = binding.rationaleContent.paddingLeft
+        val contentPaddingTop = binding.rationaleContent.paddingTop
+        val contentPaddingRight = binding.rationaleContent.paddingRight
+        val contentPaddingBottom = binding.rationaleContent.paddingBottom
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.rationaleScroll) { _, windowInsets ->
+            val systemBars = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            binding.rationaleContent.updatePadding(
+                left = contentPaddingLeft + systemBars.left,
+                top = contentPaddingTop + systemBars.top,
+                right = contentPaddingRight + systemBars.right,
+                bottom = contentPaddingBottom + systemBars.bottom,
+            )
+            windowInsets
+        }
+
+        ViewCompat.requestApplyInsets(binding.rationaleScroll)
     }
 }
